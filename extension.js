a17e7432a77e35eceabc77d41bbe4a217fcf7d21
Egor Vorontsov <sdoregor@sdore.me>
2022-03-11 10:54:55 +0300

    Added power unlock; fixed issues. Bumped version to v3.

--- extension.js
+++ extension.js
@@ -1,6 +1,6 @@
 // Huawei WMI controls
 
-const {St, Gio, GObject} = imports.gi;
+const {St, Gio, Meta, Shell, GObject} = imports.gi;
 
 const Main = imports.ui.main;
 const PanelMenu = imports.ui.panelMenu;
@@ -59,15 +59,71 @@ class HuaweiWmiIndicator extends PanelMenu.Button { // TODO: move to system batt
 			fn_lock.connect('toggled', (item, state) => this._set_fn_lock(state));
 		}; this.menu.addMenuItem(fn_lock);
 
+		let power_unlock = this._power_unlock = new NonClosingPopupSwitchMenuItem(_("Power unlock"), false); {
+			power_unlock.connect('toggled', (item, state) => this._set_power_unlock(state));
+		}; this.menu.addMenuItem(power_unlock);
+
 		this.menu.connect('open-state-changed', () => this._bpm.activate());
+
+		this._bind_keys();
 
 		this._set_bpm(null, null);
 		this._set_fn_lock(null);
+		this._set_power_unlock(null);
+	}
+
+	_destroy() {
+		this._unbind_keys();
+	}
+
+	_bind_keys() {
+		let settings = ExtensionUtils.getSettings("org.gnome.shell.extensions.huawei-wmi");
+
+		Main.wm.addKeybinding('hwmi-config', settings, Meta.KeyBindingFlags.NONE, Shell.ActionMode.ALL, this.menu.toggle.bind(this.menu));
+		Main.wm.addKeybinding('hwmi-power-unlock', settings, Meta.KeyBindingFlags.NONE, Shell.ActionMode.ALL, this._key_power_unlock.bind(this));
+	}
+
+	_unbind_keys() {
+		Main.wm.removeKeybinding('hwmi-config');
+		Main.wm.removeKeybinding('hwmi-power-unlock');
+	}
+
+	_key_power_unlock() {
+		let icon, text;
+
+		let old_state = this._power_unlock.state;
+		this._set_power_unlock();
+
+		if (this._power_unlock.state === old_state) {
+			icon = 'battery-caution-symbolic';
+			text = _("Power Unlock\nunavailable on battery");
+		} else {
+			switch (this._power_unlock.state) {
+				case true: {
+					icon = 'power-profile-performance-symbolic';
+					text = _("Performance Mode");
+				}; break;
+
+				case false: {
+					icon = 'power-profile-balanced-symbolic';
+					text = _("Balanced Mode");
+				}; break;
+
+				default: return;
+			}
+		}
+
+		Main.osdWindowManager.show(-1, Gio.icon_new_for_string(icon), text);
 	}
 
 	_set_bpm(low, high) {
 		let file = Gio.File.new_for_path("/sys/devices/platform/huawei-wmi/charge_control_thresholds");
-		if (low || high) file.replace_contents(`${low} ${high}`, null, false, 0, null);
+
+		if (low || high)
+			try {
+				file.replace_contents(`${low} ${high}`, null, false, 0, null);
+			} catch (e) {}
+
 		[low, high] = ByteArray.toString(file.load_contents(null)[1]).split(' ').map(Number);
 
 		this._bpm.label.set_text(this._BPM + `: ${low}%-${high}%`);
@@ -75,10 +131,28 @@ class HuaweiWmiIndicator extends PanelMenu.Button { // TODO: move to system batt
 
 	_set_fn_lock(state) {
 		let file = Gio.File.new_for_path("/sys/devices/platform/huawei-wmi/fn_lock_state");
-		if (state !== null) file.replace_contents(Number(state).toString(), null, false, 0, null);
+
+		if (state !== null)
+			try {
+				file.replace_contents(Number(state).toString(), null, false, 0, null);
+			} catch (e) {}
+
 		state = Boolean(Number(ByteArray.toString(file.load_contents(null)[1])));
 
 		this._fn_lock.setToggleState(state);
+	}
+
+	_set_power_unlock(state) {
+		let file = Gio.File.new_for_path("/sys/devices/platform/huawei-wmi/power_unlock");
+
+		if (state !== null)
+			try {
+				file.replace_contents(Number(state).toString(), null, false, 0, null);
+			} catch (e) {}
+
+		state = Boolean(Number(ByteArray.toString(file.load_contents(null)[1])));
+
+		this._power_unlock.setToggleState(state);
 	}
 });
 
@@ -103,5 +177,5 @@ function init(meta) {
 	return new Extension(meta.uuid);
 }
 
-// by Sdore, 2021
-// apps.sdore.me
+// by Sdore, 2021-2022
+//   apps.sdore.me
